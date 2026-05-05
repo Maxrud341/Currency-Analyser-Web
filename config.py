eea390d71594db0277f8a1106fc22020ce605d7f
Maxrud341 <mrmaximus341@gmail.com>
2026-05-05 20:59:46 +0200

    edit db for ci dc

--- config.py
+++ config.py
@@ -2,11 +2,13 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret')
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://"
-        f"{os.environ.get('POSTGRES_USER')}:"
-        f"{os.environ.get('POSTGRES_PASSWORD')}@db:5432/"
-        f"{os.environ.get('POSTGRES_DB')}"
-    )
+
+    uri = os.environ.get("DATABASE_URL")
+
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
